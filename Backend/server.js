1f89e10208687fb28be2be423580d78f71c22d21
AdityaPatilxx <161197993+AdityaPatilxx@users.noreply.github.com>
2025-03-12 17:09:42 +0000

    Add questions routes and integrate with server

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const connectDB = require('./db')
+const connectDB = require("./db");
+const questionsRoutes = require("./routes/questionsRoutes");
 require("dotenv").config();
 
-connectDB()
+connectDB();
 
 const app = express();
 app.use(express.json());
+
+app.use("/questions", questionsRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
